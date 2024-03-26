6db378bf6c6386071759c54016fb81bb86a18ef2
Ernandes Figueiredo <144298886+ernandes2023@users.noreply.github.com>
2024-03-26 02:12:45 -0300

    construcao da tela de operacao

---
+++ Componente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventoryControl
+{
+    class Componente
+    {
+        public int id;
+        public string nome;
+
+        public Componente(int id, string nome)
+        {
+            this.id = id;
+            this.nome = nome;
+        }
+    }
+}
---
+++ Operacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventoryControl
+{
+    class Operacao
+    {
+        public string module;
+        public string componente;
+        public int qtdComponente;
+        public DateTime data;
+    }
+}
